b93239b7fa9944fc531d874462cdbad4c20a2b7b
Meripip <meripip100@gmail.com>
2023-10-23 07:36:27 -0300

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CryptoWalletFrontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
